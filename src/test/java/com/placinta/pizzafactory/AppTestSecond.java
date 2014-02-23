bbd15483eccd82e66027111fc95c19a8e79d0f75
cplacinta <kristian.placinta@gmail.com>
2014-02-23 18:33:55 +0200

    Initial commit

---
+++ src/test/java/com/placinta/pizzafactory/AppTestSecond.java
@@ -0,0 +1,15 @@
+package com.placinta.pizzafactory;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple PizzaMaker.
+ */
+@Test
+public class AppTestSecond {
+
+  public void testNothing() {
+
+  }
+
+}
